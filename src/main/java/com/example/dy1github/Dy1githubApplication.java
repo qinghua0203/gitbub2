b84c05eccf9dff5c04fdb10c324d355cd65355a4
lenovo <577027461@qq.com>
2021-05-21 19:02:40 +0800

    第二年瓷提交

---
+++ src/main/java/com/example/dy1github/Dy1githubApplication.java
@@ -0,0 +1,13 @@
+package com.example.dy1github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dy1githubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dy1githubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dy1github/Dy1githubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dy1github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dy1githubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
